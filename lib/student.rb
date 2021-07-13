f3d2c1aafdb0a65d4155728c2dd9a41821e5a81b
lisswart <arnoudlisa@gmail.com>
2021-07-13 07:10:05 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(str_of_knowledge)
+        @knowledge << str_of_knowledge
+    end
 
 end
